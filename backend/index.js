5e36cb49dc94b02ba5b6f3937b865841006bf005
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-06-15 13:00:02 +0530

    production ready

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,13 @@ import clientfetch from "./routes/clientfetch.route.js";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import excelRoutes from "./routes/data.route.js";
-
-
+import path from "path";
 
 dotenv.config();
 
-
-
-
-
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
@@ -27,6 +21,16 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/excel", excelRoutes);
 app.use("/api/clients",clientfetch )
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  } );
+}
+  
+
 
 
 
